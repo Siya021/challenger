c74a61ded5a18d181b0d237b7d56c7c78504acce
Siya021 <siyamthandamazokwana@gmail.com>
2023-08-18 10:50:50 +0200

    Updates

--- challenger_gui/src/App.vue
+++ challenger_gui/src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container-fluid">
+    <NavBar/>
+    <main>
+      <router-view/>
+    </main>
+    <FooterComp/>
+  </div>
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import FooterComp from './components/FooterComp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    FooterComp
+  }
+}
+</script>
 
 <style>
 #app {
